f1232117f3d8e66d26527f153f37f786d2f4691f
injeChoi <2015jasonc@gmail.com>
2021-06-02 22:27:14 +0900

    MessageKit 도입

--- DamannaFront/DamannaFront/ChatRoom.swift
+++ DamannaFront/DamannaFront/ChatRoom.swift
@@ -8,38 +8,39 @@
 import UIKit
 import StompClientLib
 
-class ChatRoom: UIViewController, StompClientLibDelegate, UITextFieldDelegate {
+class ChatRoom: UIViewController, UITextFieldDelegate {
     
-    // Stomp로 웹소켓 서버 연결을 위한 metadata
+    // MARK: Stomp로 웹소켓 서버 연결을 위한 metadata
     let url = NSURL(string: "wss://damanna.herokuapp.com/ws/websocket")
     let intervalSec = 1.0
-    var roomID: String = "asd"
+    var roomID: String = "temp"
     public var socketClient = StompClientLib()
     
-    // RoomSelect에서 채팅방 주제를 전달받기 위한 프로퍼티
+    // RoomSelect 클래스에서 채팅방 주제를 전달받기 위한 프로퍼티
     var roomTopic: String = ""
+    
+    // 채팅들이 보여질 테이블 뷰
+    @IBOutlet var chatTable: UITableView!
     
     // 사용자에게 메시지 입력받는 Text Field
     @IBOutlet var userMessage: UITextField!
     
-    // 뷰가 메모리에 로드될 때 stomp client에서 연결 시작 및 초기 설정
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("1")
-        socketClient.openSocketWithURLRequest(request: NSURLRequest(url: url! as URL), delegate: self)
-        print("2")
-        print("\(socketClient)")
-        print("3")
-        
-        // 채팅방 이름 설정
-        self.navigationItem.title = roomTopic + " 채팅방"
         
         // TextField Delegation
         self.userMessage.delegate = self
         
+        // 뷰가 메모리에 로드될 때 stomp client에서 연결 시작 및 초기 설정
+        socketClient.openSocketWithURLRequest(request: NSURLRequest(url: url! as URL), delegate: self)
+        
+        // 채팅방 이름 설정
+        self.navigationItem.title = roomTopic + " 채팅방"
+        
         // subscribe 할 때 사용자가 선택한 방 이름 전송
         roomID = roomTopic
+        
+        
     }
     
     // return key 누르면 키보드 사라지게
@@ -48,16 +49,13 @@ class ChatRoom: UIViewController, StompClientLibDelegate, UITextFieldDelegate {
         print("return key has entered")
         return true
     }
-    
-    
-    
-    
-    
-    
-    
    
-    // MARK: Stomp 시작
-    // json 형식으로 data 전달을 위한 구조체
+}
+
+
+// MARK: Stomp 시작
+extension ChatRoom: StompClientLibDelegate {
+    // JSON 형식으로 data 전달을 위한 구조체
     struct chatMessage : Codable {
         var type: String? = ""
         var content: String? = ""
@@ -112,7 +110,7 @@ class ChatRoom: UIViewController, StompClientLibDelegate, UITextFieldDelegate {
     func serverDidSendError(client: StompClientLib!, withErrorMessage description: String, detailedErrorMessage message: String?) {
         print("Error Send : \(String(describing: message))")
         if(!socketClient.isConnected()) {
-           // reconnect()
+            // reconnect()
         }
     }
     
@@ -120,5 +118,5 @@ class ChatRoom: UIViewController, StompClientLibDelegate, UITextFieldDelegate {
         print("Server ping")
     }
     
-    
 }
+
