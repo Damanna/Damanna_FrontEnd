73bf546795dfe1def6e21c5a299650b6426e4c0e
injeChoi <2015jasonc@gmail.com>
2021-06-03 17:19:07 +0900

    add tag function

--- DamannaFront/DamannaFront/RoomSelect.swift
+++ DamannaFront/DamannaFront/RoomSelect.swift
@@ -9,77 +9,65 @@ import UIKit
 
 class RoomSelect: UITableViewController {
     
-    var roomList = [String]()
-    var topic: String = ""
+    var roomList = [RoomVO]()
+    var topic = RoomVO()
     
     // POST 할 데이터 정의 
     struct PostComment: Codable {
         let roomID: String
-    }
-    
-    override func viewDidLoad() {
-        
-//        let url: URL! = URL(string: "https://damanna.herokuapp.com/room/read")
-//        let dbData = try! Data(contentsOf: url)
-//        let log = NSString(data: dbData, encoding: String.Encoding.utf8.rawValue) ?? "No Data"
-//        NSLog("API Result = \(log)")
-//
-//        do {
-//            // DB에서 가져온 json객체를 NSArray로 변환
-//            let data = try JSONSerialization.jsonObject(with: dbData, options: []) as! NSArray
-//
-//            // 변환한 데이터에서 객체 하나씩 뽑아서 String으로 캐스팅 후 리스트에 추가
-//            for row in data {
-//                let r = row as! NSDictionary
-//
-//                roomList.append(r["roomID"] as! String)
-//            }
-//
-//        } catch {
-//            NSLog("Parse Error!")
-//        }
+        let tags: String
     }
     
     // MARK:- 주제 전달
     // Segue perform 전에 미리 해야할 일 정의, 선택한 주제 전달
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let dest = segue.destination
-        
-        // ChatRoom 이름 설정을 위해 주제 전달 
-        guard let chatroomVC = dest as? ChatRoom else {
-            return
-        }
-        
-        chatroomVC.roomTopic = self.topic
-    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let dest = segue.destination
+//
+//        // ChatRoom 이름 설정을 위해 주제 전달
+//        guard let chatroomVC = dest as? ChatRoom else {
+//            return
+//        }
+//
+//        chatroomVC.roomTopic = self.topic
+//    }
     
     @IBAction func addRoom(_ sender: Any) {
         var postRoomID: String = ""
+        var postTag: String = ""
         
         // 알림창 객체 생성
-        let alert = UIAlertController(title: "새로운 주제 입력", message: "추가하고 싶은 대화 방의 주제를 입력하세요", preferredStyle: .alert)
+        let alert = UIAlertController(title: "새로운 채팅방 만들기", message: "채팅방의 설명을 작성하세요", preferredStyle: .alert)
         
         // 알림창에 입력 폼 추가
         alert.addTextField() { (tf) in
-            tf.placeholder = "주제를 입력하세요"
+            tf.placeholder = "방 이름을 입력하세요"
+        }
+        alert.addTextField() { (tf) in
+            tf.placeholder = "방 설명을 입력하세요"
         }
         
         // OK 버튼 객체 생성
         let ok = UIAlertAction(title: "추가하기", style: .default) { (_) in
             // 알림창의 0번째 입력 필드에 값이 있다면
-            if let title = alert.textFields?[0].text {
-                // TextField에서 입력 받은 값을 JSON 객체로 날릴 변수에 저장
-                postRoomID = title
+            
+            let rvo = RoomVO()
+            
+            let title = alert.textFields?[0].text
+            let tag = alert.textFields?[1].text
+            
+            if (title != nil), (tag != nil) {
+                postRoomID = title!
+                postTag = tag!
                 
                 // MARK: POST 하기
                 // JSON 객체 준비
-                let comment = PostComment(roomID: postRoomID)
+                let comment = PostComment(roomID: postRoomID, tags: postTag)
                 guard let uploadData = try? JSONEncoder().encode(comment) else {
                     return
                 }
                 
                 // POST request 만들기
-                let url: URL! = URL(string: "https://damanna.herokuapp.com/room/save")
+                let url: URL! = URL(string: "https://test-message2.herokuapp.com/room/save")
                 var request = URLRequest(url: url)
                 request.httpMethod = "POST"
                 request.setValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -98,7 +86,10 @@ class RoomSelect: UITableViewController {
                 task.resume()
                 
                 // 배열에 입력된 값을 추가하고 테이블 갱신
-                self.roomList.append(title)
+                rvo.Topic = title
+                rvo.roomTag = tag
+                
+                self.roomList.append(rvo)
                 self.tableView.reloadData()
             }
         }
@@ -121,23 +112,25 @@ class RoomSelect: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let row = self.roomList[indexPath.row] // 행 번호를 알고자 할 때는 indexPath.row
+        
         // "cell" 아이디를 가진 셀을 읽어온다. 없으면 UITableViewCell 인스턴스를 생성한다
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "roomCell") as! RoomCell
         
         // 셀의 기본 텍스트 레이블 행 수 제한을 없앤다
-        cell.textLabel?.numberOfLines = 0
+        //cell.textLabel?.numberOfLines = 0
         
         // 셀의 기본 텍스트 레이블에 배열 변수의 값을 할당한다
-        cell.textLabel?.text = roomList[indexPath.row]
+        cell.Topic?.text = row.Topic
+        cell.roomTag?.text = row.roomTag
+        
         return cell
     }
     
 //    테이블 뷰에 행이 하나 추가되어 reload() 되어 뷰가 나타날 떄 cell의 크기를 자동으로 글자 수의 길이에 맞춰 늘림 (근데 없어도 잘 된다..?)
     override func viewWillAppear(_ animated: Bool) {
-        self.tableView.estimatedRowHeight = 50
-        self.tableView.rowHeight = UITableView.automaticDimension
         
-        let url: URL! = URL(string: "https://damanna.herokuapp.com/room/read")
+        let url: URL! = URL(string: "https://test-message2.herokuapp.com/room/read")
         let dbData = try! Data(contentsOf: url)
         let log = NSString(data: dbData, encoding: String.Encoding.utf8.rawValue) ?? "No Data"
         NSLog("API Result = \(log)")
@@ -146,16 +139,20 @@ class RoomSelect: UITableViewController {
             // DB에서 가져온 json객체를 NSArray로 변환
             let data = try JSONSerialization.jsonObject(with: dbData, options: []) as! NSArray
             
+            roomList.removeAll()
             // 변환한 데이터에서 객체 하나씩 뽑아서 String으로 캐스팅 후 리스트에 추가
             for row in data {
                 let r = row as! NSDictionary
-                
-                roomList.append(r["roomID"] as! String)
+                let rvo = RoomVO()
+                rvo.Topic = r["roomID"] as? String
+                rvo.roomTag = r["tags"] as? String
+                roomList.append(rvo)
             }
             
         } catch {
             NSLog("Parse Error!")
         }
+        self.tableView.reloadData()
     }
     
     // 선택한 행(방 주제)에 대해 일어나는 event 정의
@@ -166,7 +163,7 @@ class RoomSelect: UITableViewController {
         // performSegue(withIdentifier: "moveToChatRoom", sender: self)
         
         let vc = ChatViewContoller()
-        vc.roomTopic = self.topic
+        vc.roomTopic = self.topic.Topic!
         navigationController?.pushViewController(vc, animated: true)
     }
     
---
+++ DamannaFront/DamannaFront/RoomVO.swift
@@ -0,0 +1,13 @@
+//
+//  RoomVO.swift
+//  DamannaFront
+//
+//  Created by InJe Choi on 2021/06/03.
+//
+
+import Foundation
+
+class RoomVO {
+    var Topic: String?
+    var roomTag: String?
+}
